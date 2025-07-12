31efd3bf3975b5681062abba1e54caab31872318
iris LUO <nianyiLuo2023@163.com>
2025-07-12 09:47:07 -0700

    fix: vercel deploy error

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: '/',
   server: {
     port: 3000,
     open: true
